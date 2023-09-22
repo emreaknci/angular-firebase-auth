b6b89688bc9048bd0e23b00c9374a0344d8ca948
Yunus Emre Akıncı <emreakinci696@gmail.com>
2023-09-22 11:32:59 +0300

    .

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC1N5P6oFxKD17vSxg3fMO5_pwVMZMUmmI",
+    authDomain: "angularfiebase-authentic-e388d.firebaseapp.com",
+    projectId: "angularfiebase-authentic-e388d",
+    storageBucket: "angularfiebase-authentic-e388d.appspot.com",
+    messagingSenderId: "653189426392",
+    appId: "1:653189426392:web:eaef87186c2487bd5b7bad"
+  },
+};
